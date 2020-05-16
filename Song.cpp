fa8e03d8f805452e900c26aa2936fd0d1f7ec42a
empty-works <pama.mark@gmail.com>
2020-05-16 04:54:04 +0200

    Function prototypes for overloaded operators.

--- Song.cpp
+++ Song.cpp
@@ -1,3 +1,5 @@
 #include "Song.h"
 
-
+std::ostream &operator<<(std::ostream &os, const Song &obj) {
+	 
+}
--- Song.h
+++ Song.h
@@ -1,8 +1,12 @@
 #ifndef SONG_H_
 #define SONG_H_
+#include <iostream>
 #include <string>
+#include <iomanip>
 
 class Song {
+
+	friend std::ostream &operator<<(std::ostream &os, const Song &obj);
 	private:
 		std::string name_ {};
 		std::string title_ {};
@@ -10,7 +14,10 @@ class Song {
 	public:
 		std::string GetName() {return name_;}
 		std::string GetTitle() {return title_;}
-		int GetRating() {return rating_;}		
+		int GetRating() {return rating_;}
+		
+		bool operator<(const Song &rhs);
+		bool operator==(const Song &rhs);		
 };
 
 #endif
