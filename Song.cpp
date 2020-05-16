ba8c46a2036ce9641e70b6128c27ba3c6d27988c
empty-works <pama.mark@gmail.com>
2020-05-16 06:08:40 +0200

    Implemented < and == overloaded operators.

--- Song.cpp
+++ Song.cpp
@@ -6,3 +6,11 @@ std::ostream &operator<<(std::ostream &os, const Song &obj) {
 		<< std::setw(5) << std::left << obj.GetRating();
 	return os;	
 }
+
+bool Song::operator<(const Song &rhs) {
+	return this->name_ < rhs.GetName();	
+}
+
+bool Song::operator==(const Song &rhs) {
+	return (this->name_ == rhs.GetName() && this->artist_ == rhs.GetArtist());
+}	
