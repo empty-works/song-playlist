9c0dfa500ceed2108e244dd387c897d17eadf3b5
empty-works <pama.mark@gmail.com>
2020-05-16 06:27:55 +0200

    Implemented constructors.

--- Song.h
+++ Song.h
@@ -12,6 +12,9 @@ class Song {
 		std::string artist_ {};
 		int rating_ {};
 	public:
+		Song() = default;
+		Song(std::string name, std::string artist, int rating) 
+			:name_ {name}, artist_ {artist}, rating_ {rating} {}
 		std::string GetName() const {return name_;}
 		std::string GetArtist() const {return artist_;}
 		int GetRating() const {return rating_;}
